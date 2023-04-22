f7ee3a3f21b8f2996be3e51e79a98df33156d9ae
chikitingo1 <20100770@ue.edu.pe>
2023-04-22 07:46:41 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// obtenemos el botón por su ID
+const btn1 = document.getElementById("btn1");
+
+// agregamos el evento de clic
+btn1.addEventListener("click", function() {
+  // redirigimos a la página deseada
+  window.location.href = "https://github.com/";
+});
